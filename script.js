1f901e876bee673e4526f03b3479bd1547d3d5cd
aayush <hiaayush30@gmail.com>
2025-03-18 01:34:30 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,62 @@
+let todos = localStorage.getItem('todos');
+if (!todos) {
+    todos = []
+} else {
+    todos = JSON.parse(todos);
+}
+
+let darkMode = true;
+
+const title = document.querySelector('#title');
+const desc = document.querySelector('#desc');
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(title.value)
+    if (title.value.length == 0) {
+        return alert('title cannot be empty!')
+    }
+    if (desc.value.length == 0) {
+        return alert('description cannot be empty!');
+    }
+    todos.push({
+        id:Math.random(),
+        title: title.value,
+        desc: desc.value,
+    })
+    localStorage.setItem('todos', JSON.stringify(todos));
+    alert('todo added!');
+    title.value = '';
+    desc.value = '';
+}
+
+const mode = document.querySelector('#mode');
+const navbar = document.querySelector('#navbar');
+const main = document.querySelector('#main');
+const form= document.querySelector('#form');
+
+const handleMode = () => {
+    if (darkMode) {
+        document.body.classList.remove('dark');
+        main.classList.remove('dark');
+        navbar.classList.remove('dark');
+        form.classList.remove('dark');
+    } else {
+        document.body.classList.add('dark');
+        main.classList.add('dark');
+        navbar.classList.add('dark');
+        form.classList.add('dark');
+    }
+    darkMode = !darkMode;
+}
+mode.addEventListener('click',handleMode);
+
+
+const logo = document.querySelector('#logo');
+const text = "Todo Manager"
+
+text.split('').forEach((char,index)=>{
+    setTimeout(()=>{
+      logo.innerHTML+=logo.innerHTML=char 
+    },500+index*150)
+})
---
+++ script2.js
@@ -0,0 +1,63 @@
+let todos = localStorage.getItem('todos');
+if(!todos){
+    todos=[];
+}else{
+    todos=JSON.parse(todos);
+}
+let darkMode = true;
+
+const noTodos = document.querySelector('.noTodos');
+if(todos.length!=0) noTodos.innerHTML=""
+const main = document.querySelector('#main');
+
+todos.forEach(todo => {
+    const box = document.createElement('div');
+    box.classList.add('box');
+
+    const title = document.createElement('h3');
+    title.innerHTML = todo.title;
+    box.appendChild(title);
+
+    const desc = document.createElement('p');
+    desc.innerHTML = todo.desc;
+
+    const deleteSpan = document.createElement('span');
+    deleteSpan.classList.add('delete');
+    deleteSpan.innerHTML="delete"
+    deleteSpan.addEventListener('click',()=>{
+        todos=todos.filter(item=>item.id!=todo.id);
+        localStorage.setItem('todos',JSON.stringify(todos));
+        window.location.reload();
+    })
+    box.appendChild(deleteSpan);
+
+    if(darkMode) box.classList.add('dark');
+    box.appendChild(desc);
+    main.appendChild(box);
+});
+
+
+const mode = document.querySelector('#mode');
+const navbar = document.querySelector('#navbar');
+const boxes = document.querySelectorAll(".box");
+
+const handleMode = () => {
+    console.log("wjhie")
+    if (darkMode) {
+        document.body.classList.remove('dark');
+        main.classList.remove('dark');
+        navbar.classList.remove('dark');
+        boxes.forEach(box=>{
+            box.classList.remove('dark');
+        })
+    } else {
+        document.body.classList.add('dark');
+        main.classList.add('dark');
+        navbar.classList.add('dark');
+        boxes.forEach(box=>{
+            box.classList.add('dark');
+        })
+    }
+    darkMode = !darkMode;
+}
+mode.addEventListener('click',handleMode);
